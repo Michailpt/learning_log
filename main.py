9cff997531f99b1e7f55df2e2ed4af3d692a78eb
Kraimon <sdgdg@gmail.com>
2024-05-13 13:41:50 +0300

    add file 2

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('sdf')
+# НЕ ИЗМЕНЯЕМЫЕ (Не Сохраняемые)
+y='abc'
+z=y  #y и z ссылаются на один объект 'abc', но изменить его по ссылке не смогут
+z.upper() # не сработает, т.к неизменяемый объект
+w=z.upper() # срабоатает
+print(y, id(y))
+print(z, id(z))
+print(w, id(w))
+p='abc'
+o='abc'
+print(p, id(p))
+print(o, id(o))
+print(w, id(w))
+D= [1,2,3,4,5,6,7,8,9]
+for i in D:
+    print(i, id)
+def fff():
+    return
+fff()
